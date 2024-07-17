617fa5fb948ffe79424128c91931051112a79913
Amankr2099 <imamankr2121@gmail.com>
2024-07-17 20:17:02 +0530

    add github

--- src/pages/AboutSection.jsx
+++ src/pages/AboutSection.jsx
@@ -7,7 +7,7 @@ export const AboutSection = () => {
         <div className="col-md-8 offset-md-2 p-4">
           <h2 className="mt-5 mb-3">About Cosmos</h2>
           <p>
-          Welcome to our Cosmolog, where we embark on the extraordinary journey through the vast expanse of the universe. At our core, we are a passionate collective of astronomers, astrophysicists, and science enthusiasts, eager to share the wonders of the universe with you. Our mission is to unravel the mysteries of space and time, providing you with a celestial tapestry of articles that illuminate the beauty and complexity of the cosmos. From the birth of stars to the enigmatic depths of black holes, we strive to make the wonders of space accessible to everyone, fostering a deep appreciation for the boundless wonders of the universe. Join us on this captivating voyage as we delve into the infinite realms of the cosmos, one article at a time.
+          Welcome to our Cosmolog, where we embark on an extraordinary journey through the vast expanse of the universe. At our core, we are a passionate collective of astronomers, astrophysicists, and science enthusiasts, eager to share the wonders of the universe with you. Our mission is to unravel the mysteries of space and time, providing you with a celestial tapestry of articles that illuminate the beauty and complexity of the cosmos. From the birth of stars to the enigmatic depths of black holes, we strive to make the wonders of space accessible to everyone, fostering a deep appreciation for the boundless wonders of the universe. Join us on this captivating voyage as we delve into the infinite realms of the cosmos, one article at a time.
           </p>
           {/* <h4 className="mt-5 mb-3">Our Team</h4>
           <p>
